2984827663b9d00f889f1550aea1c932e89a6cf4
TheBigL <lmohamed1@studentmail.nait.ca>
2022-01-13 00:10:00 -0700

    I've finished most of the coding. I've added the basic Jukebot functions. I plan to add more commands in the future. For now, I'm focuses on fixing a technical issue at the moment.

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import discord
 import os
 from discord.ext import commands
 from dotenv import load_dotenv
-from discord.utils import get
-from discord import FFmpegPCMAudio
-from discord import TextChannel
-from youtube_dl import YoutubeDL
+import music
+
+cogs = [music]
+client = commands.Bot(command_prefix='.', intents=discord.Intents.all())
+
+for i in range(len(cogs)):
+    cogs[i].setup(client)
+
 
 load_dotenv()
-client = commands.Bot(command_prefix='.')
-intents = discord.Intents.default()
-intents.members = True
-
 # Run the Bot
 client.run(os.getenv('TOKEN'))
