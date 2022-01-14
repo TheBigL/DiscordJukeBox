4a0a4520b86a767cf9aa8474ffd6e0b0ebe6b196
TheBigL <lmohamed1@studentmail.nait.ca>
2022-01-14 00:38:11 -0700

    Building the methods needed and restructuring the music.py code.

--- YTDLSource.py
+++ YTDLSource.py
@@ -2,13 +2,11 @@ import asyncio
 import discord
 import youtube_dl
 
-from discord.ext import commands
-
 youtube_dl.utils.bug_reports_message = lambda: ''
 
 ffmpeg_options = {'before_options': '-reconnect 1 -reconnect_streamed 1 '
                                     '-reconnect_delay_max 5', 'options': '-vn'}
-ydl_options = {'format': 'bestaudio'}
+ydl_options = {'format': 'best-audio'}
 
 ytdl = youtube_dl.YoutubeDL(ydl_options)
 
@@ -22,13 +20,13 @@ class YTDLSource(discord.PCMVolumeTransformer):
         self.title = data.get('title')
         self.url = data.url('url')
 
-        @classmethod
-        async def from_url(cls, url, *, loop=None, stream=False):
-            loop = loop or asyncio.get_event_loop()
-            data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, not stream))
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, not stream))
 
-            if 'entries' in data:
-                data = data['entries'][0]
+        if 'entries' in data:
+            data = data['entries'][0]
 
-            filename = data['url'] if stream else ytdl.prepare_filename(data)
-            return cls(discord.FFmpegOpusAudio(filename, **ffmpeg_options), data)
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegOpusAudio(filename, **ffmpeg_options), data=data)
