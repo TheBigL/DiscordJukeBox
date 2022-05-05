e2b8a6ecb63f11d2a52ac2c3c3e5a49247471d8d
LM <lmohamed1@studentmail.nait.ca>
2022-05-05 11:19:24 -0600

    Reformatted code slightly

--- music.py
+++ music.py
@@ -1,11 +1,5 @@
-import asyncio
-
-import discord
 import youtube_dl
-import yt
 from discord.ext import commands
-from discord.ext.commands import bot
-
 import YTDLSource
 
 youtube_dl.utils.bug_reports_message = lambda: ''
